a5fd93d4398a4be688245bc816224ce246637391
mx123 <maxunmnm12@163.com>
2019-10-06 19:13:25 +0800

    第四次提交

--- one/app.py
+++ one/app.py
@@ -60,6 +60,43 @@ def reg():
         return render_template("./home/login.html")
 
 
+@app.route("/reg1", methods=["GET", "POST"])
+def reg1():
+    if request.method == "GET":
+        return render_template("./home/reg.html")
+    elif request.method == "POST":
+        uname = request.form.get("uname1")
+        pwd = request.form.get("password3")
+        pwd2 = request.form.get("password4")
+
+        if not re.match("^[1][3,4,5,7,8][0-9]{9}$",uname):
+
+            return "注册失败，用户名格式错误！"
+        if not re.match("^[0-9]{1}([a-zA-Z0-9]|[._]){6,15}$", pwd):
+            return "密码格式错误"
+        if pwd != pwd2:
+            return "密码输入不一致"
+        # if not re.match("^[1][3,4,5,7,8][0-9]{9}$", phone):
+        #     return "手机格式不对"
+
+        # if not user_reg_login.user_reg(uname, pwd, phone, email):
+        #     # 注册失败
+        #     return "注册失败，用户名已经存在，请换一个用户名试一下！"
+        # else:
+        #     # 注册成功
+        try:
+            cur = db.cursor()
+            cur.execute(
+                "INSERT INTO sp_user VALUES (default, %s, md5(%s), sysdate(), sysdate(), '1', '1')", (uname, uname + pwd))
+            cur.close()
+            db.commit()
+        except:
+            abort(Response("用户注册失败！"))
+
+        # 注册成功就跳转到登录页面
+
+        return render_template("./home/login.html")
+
 @app.route("/user_center", methods=["GET", "POST"])
 def user_center():
     if request.method == "GET":
